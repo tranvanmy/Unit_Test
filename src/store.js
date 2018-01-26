c9f36b76cfd4586db448641dcdd12cf69040dc73
tranvanmy <anhmynd19101995@gmail.com>
2018-01-26 16:58:36 +0700

    Vuex

---
+++ src/store.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        //TODO
+    },
+
+    getters: {
+        //TODO: implement getter 
+    },
+
+    mutations: {
+        // TODO: implement mutation
+    },
+
+    actions:{
+
+    }
+})
